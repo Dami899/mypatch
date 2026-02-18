37106f3874c05fda7814871eea3ca0fc233dbe0a
MokaAkashiya85 <ThaERROR404@gmail.com>
2026-02-18 19:17:03 +0100

    owo

--- lua/unitvehicles/cl_menu_pnotes.lua
+++ lua/unitvehicles/cl_menu_pnotes.lua
@@ -12,12 +12,15 @@ UV.PNotes = {
 Date = { year = 2026, month = 2, day = 20 },
 Text = [[
 **New Features**
+- Added two new Pursuit Techs:
+ |-- Racers: [string:uv.ptech.ghost] - Become non-collidable with props and other vehicles for a short time
+ |-- Units: [string:uv.ptech.grappler] - Grab a fleeing vehicle's wheels and hold them in place!
 - Racing: Added support for *AI Path Nodes*
  |-- When included, AI Racers will follow these paths instead of checkpoints
  |-- Path Nodes support multi-path racing, where the AI will now randomly pick a route
  |-- *Curve Strength* allows the user to apply a gradual turn for longer paths
 - Added support for themed Police Scanners
- |-- Only Most Wanted has its themed scanner for the time being
+ |-- Only *Most Wanted* has its themed scanner for the time being
  |-- Also added the option to have the vehicle's forward axis be used for the scanner rather than the camera
 
 **Changes**
