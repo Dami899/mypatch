0b107cec419f473848e2f2fb6c1e9f30aa4bff95
Owner <stanleyjohnson146@gmail.com>
2026-05-27 18:02:24 +0200

    Edit pNote

--- lua/unitvehicles/cl_menu_pnotes.lua
+++ lua/unitvehicles/cl_menu_pnotes.lua
@@ -9,11 +9,14 @@ UV = UV or {}
 
 UV.PNotes = {
 ["1.7.1"] = {
-      Date = { year = 2026, month = 5, day = 27 },
-      Text = [[
-      **Fixes**
-      - Added a new toggle 
-      ]],
+Date = { year = 2026, month = 5, day = 27 },
+Text = [[
+**Additions**
+- Added a new toggle: *Workshop Priority*
+      |-- If enabled, Workshop content will be prioritized over Local content
+      |-- Disable this if you want to use/work on local content instead of Workshop content
+      |-- You will need to reload the map for the change to take effect.
+]],
 },    
 ["1.7.0"] = {
 Date = { year = 2026, month = 5, day = 27 },
